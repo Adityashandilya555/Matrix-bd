0a3b38d0f19a493115ccd8673764d67fbe52edd7
0Advi <159749709+0Advi@users.noreply.github.com>
2026-07-10 14:33:06 +0530

    chore: remove accidentally committed scratch files

--- backend/test_text.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import text
-from sqlalchemy.dialects import postgresql
-
-t = text("y \:= (elem->>'year')\:\:int;")
-compiled = t.compile(dialect=postgresql.dialect())
-print("COMPILED TEXT:")
-print(compiled.string)
